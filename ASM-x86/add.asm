71d22bc777416eb98b9c54c8498832e8e8a78d7b
P.S.Narayanan <psnappz@gmail.com>
2015-07-28 23:16:47 +0530

    Update add.asm

--- ASM-x86/add.asm
+++ ASM-x86/add.asm
@@ -20,9 +20,11 @@ mov eax,DWORD[ebp+8]
 add eax,DWORD[ebp+12]
 mov DWORD[esp],eax ;Value of eax stored at stack
 lea ecx,[esp] ;loading effective address to print
+mov eax,0x04 ;syswrite
+mov ebx,0x01 ;stdout
 mov edx,1 ;1 byte
 int 0x80 ;interrupt
 
-mov eax,0x01
+mov eax,0x01 
 mov ebx,0
 int 0x80
