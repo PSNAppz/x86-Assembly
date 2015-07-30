fdbc67ec29b16f6e73f2cb97cb051f91b5e4a73b
P.S.Narayanan <psnappz@gmail.com>
2015-07-30 20:47:47 +0530

    Update usrinpt.asm

--- ASM-x86/usrinpt.asm
+++ ASM-x86/usrinpt.asm
@@ -1,9 +1,42 @@
 SECTION .text
 global _start
 
+SECTION .data
+usrmsg db 'Enter the no'
+uml equ $-usrmsg
+outmsg db 'The no is:'
+otm equ $-outmsg
+
+SECTION .bss
+num resb 5
+
 _start:
 
 push ebp
 mov ebp,esp
-sub esp,0xc
-mov eax,0x02
+mov eax,0x04
+mov ebx,0x01
+mov ecx,usrmsg
+mov edx,uml
+int 0x80
+
+mov eax,0x03
+mov ebx,0x01
+mov ecx,num
+mov edx,uml
+int 0x80
+
+mov eax,0x04
+mov ebx,0x01
+mov ecx,outmsg
+mov edx,otm
+int 0x80
+
+mov eax,0x04
+mov ebx,0x01
+mov ecx,num
+mov edx,5
+
+mov eax,0x01
+mov ebx,0x00
+int 0x80
