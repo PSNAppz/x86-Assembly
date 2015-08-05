54b44d19d25279953cdfb1e3115960e2a9ac9762
P.S.Narayanan <psnappz@gmail.com>
2015-08-05 19:21:17 +0530

    Update sum_num.asm

--- ASM-x86/sum_num.asm
+++ ASM-x86/sum_num.asm
@@ -19,4 +19,33 @@ mov ebp,esp
 sub esp,0xc
 mov DWORD [ebp-4],0x00
 cmp DWORD [ebp+8],0x01
-jle jl1
+jle jlb
+cmp DWORD [ebp+0x8],0xfff
+jle jlc
+
+jlb:
+
+mov eax,DWORD[ebp+0x8]
+jmp
+
+jlc:
+
+mov DWORD [ebp-ox8],0x0
+jmp jld
+
+jla:
+
+mov eax,DWORD [ebp-0x8]
+add DWORD[ebp-0x4],eax
+add DWORD [ebp-0x8],0x1
+
+jld:
+
+mov eax,DWORD[ebp-0x8]
+cmp eax,DWORD [ebp+0x8]
+jle jla
+mov eax,DWORD [ebp-0x4]
+mov DWORD [esp],eax
+call foo
+leave
+ret
